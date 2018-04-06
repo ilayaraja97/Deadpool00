5dca589fa14823ac0dd5525232fbffbfd400205a
S Ilayaraja <2015kucp1030@iiitkota.ac.in>
2018-04-06 16:55:00 +0530

    cleanup

--- src/lbpDetectFace.py
+++ src/lbpDetectFace.py
@@ -1,8 +1,6 @@
 import numpy as np
 import cv2
 
-lbp_face_cascade = cv2.CascadeClassifier('data/lbpcascade_frontalface.xml')
-
 
 def detect_faces(f_cascade, colored_img, scale_factor=1.1):
     img_copy = np.copy(colored_img)
@@ -17,23 +15,3 @@ def detect_faces(f_cascade, colored_img, scale_factor=1.1):
 
     return img_copy
 
-
-cv2.namedWindow("exit on ESC")
-vc = cv2.VideoCapture(0)
-frame = 0
-if vc.isOpened():  # try to get the first frame
-    rval, frame = vc.read()
-else:
-    rval = False
-
-f = frame
-
-while rval:
-    cv2.imshow("exit on ESC", cv2.flip(f, 1))
-    rval, frame = vc.read()
-    f = detect_faces(lbp_face_cascade, frame)
-    key = cv2.waitKey(20)
-    if key == 27:  # exit on ESC
-        break
-cv2.destroyWindow("exit on ESC")
-cv2.VideoCapture.release(vc)
---
+++ tests/faceDet.py
@@ -0,0 +1,25 @@
+import cv2
+
+from src.lbpDetectFace import detect_faces
+
+lbp_face_cascade = cv2.CascadeClassifier('data/lbpcascade_frontalface.xml')
+
+cv2.namedWindow("exit on ESC")
+vc = cv2.VideoCapture(0)
+frame = 0
+if vc.isOpened():  # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+f = frame
+
+while rval:
+    cv2.imshow("exit on ESC", cv2.flip(f, 1))
+    rval, frame = vc.read()
+    f = detect_faces(lbp_face_cascade, frame)
+    key = cv2.waitKey(20)
+    if key == 27:  # exit on ESC
+        break
+cv2.destroyWindow("exit on ESC")
+cv2.VideoCapture.release(vc)
