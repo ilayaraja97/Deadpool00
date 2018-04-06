7d2cf958e0a51a67216cd75fae4695b721415c66
S Ilayaraja <2015kucp1030@iiitkota.ac.in>
2018-04-06 16:44:04 +0530

    directory restructure

---
+++ src/testEnvironment.py
@@ -0,0 +1,4 @@
+import numpy as np
+import cv2
+print("numpy "+np.__version__)
+print("opencv "+cv2.__version__)
--- test.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-import cv2
-import cv2.data
-print(np.__version__)
-print(cv2.__version__)
