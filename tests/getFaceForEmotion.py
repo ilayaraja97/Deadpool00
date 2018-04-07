50bd39a51da3248bc92de118ea5bc4435cfdfb79
S Ilayaraja <2015kucp1030@iiitkota.ac.in>
2018-04-07 19:40:19 +0530

    tests for theano and keras

---
+++ tests/getFaceForEmotion.py
@@ -0,0 +1,27 @@
+import cv2
+
+from src.lbpDetectFace import detect_faces, draw_faces, get_largest_face
+
+lbp_face_cascade = cv2.CascadeClassifier('../data/lbpcascade_frontalface.xml')
+
+cv2.namedWindow("exit on ESC")
+vc = cv2.VideoCapture(0)
+frame = 0
+if vc.isOpened():  # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+f = frame
+
+while rval:
+    cv2.imshow("exit on ESC", cv2.flip(f, 1))
+    rval, frame = vc.read()
+    temp = get_largest_face(frame, detect_faces(lbp_face_cascade, frame))
+    if temp.shape != (0, 0, 3):
+        f = temp
+    key = cv2.waitKey(20)
+    if key == 27:  # exit on ESC
+        break
+cv2.destroyWindow("exit on ESC")
+cv2.VideoCapture.release(vc)
