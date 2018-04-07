de9fce1ce4c39dbe18c96ffa23528243001a47e1
S Ilayaraja <2015kucp1030@iiitkota.ac.in>
2018-04-07 10:55:35 +0530

    tests for theano and keras

--- tests/testEnvironment.py
+++ tests/testEnvironment.py
@@ -1,4 +1,11 @@
 import numpy as np
 import cv2
+import platform
+import theano
+import keras
+
+print(platform.architecture())
 print("numpy "+np.__version__)
 print("opencv "+cv2.__version__)
+print("Theano "+theano.__version__)
+print("Keras "+keras.__version__)
