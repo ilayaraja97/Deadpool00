dfbf1229b175fdab4a33d8d1a161f82998f1a3a2
ilayaraja97 <ilayaraja97@gmail.com>
2018-04-10 22:01:52 +0530

    save point

--- tests/testEnvironment.py
+++ tests/testEnvironment.py
@@ -1,11 +1,13 @@
 import numpy as np
 import cv2
 import platform
-import theano
+import tensorflow as tf
 import keras
 
 print(platform.architecture())
 print("numpy "+np.__version__)
 print("opencv "+cv2.__version__)
-print("Theano "+theano.__version__)
 print("Keras "+keras.__version__)
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+print(sess.run(hello))
