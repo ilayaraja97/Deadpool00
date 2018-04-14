66253aa475e983de43a1621a9442868e7824dffb
himani <2015kucp1005@iiitkota.ac.in>
2018-04-14 21:25:11 +0530

    function

--- src/ck_import.py
+++ src/ck_import.py
@@ -8,44 +8,6 @@ from src.lbpDetectFace import get_largest_face, detect_faces
 
 
 def importCKPlusDataset(dir='CK+', includeNeutral=False):
-    ############################################################################
-    # Function: importCKPlusDataset
-    # Depending on preferences, this ranges from 309 - 920 images and labels
-    #    - 309 labeled images
-    #    - 18 more "Contempt" images (not in our vocabulary)
-    #    - 593 neutral images
-    #
-    # For this to work, make sure your CKPlus dataset is formatted like this:
-    # CKPlus = root (or whatever is in your 'dir' variable)
-    # CKPlus/CKPlus_Images = Root for all image files (no other file types here)
-    #    Example image path:
-    #    CKPlus/CKPlus_Images/S005/001/S005_001_00000011.png
-    #
-    # CKPlus/CKPlus_Labels = Root for all image labels (no other file types)
-    #    Example label path:
-    #    CKPlus/CKPlus_Labels/S005/001/S005_001_00000011_emotion.png
-    #
-    # CKPlus/* - anything else in this directory is ignored, as long as it
-    # is not in the _Images or _Labels subdirectories
-    #
-    # Optional inputs:
-    # dir - Custom root directory for CKPlus dataset (if not 'CKPlus')
-    #
-    # includeNeutral - Boolean to include neutral pictures or not
-    #    Note: Every sequence begins with neutral photos, so neutral photos
-    #    greatly outnumber all other combined (approximately 593 to 327)
-    #
-    # contemptAs - Since it's not in our vocabulary, by default all pictures
-    # labeled "Contempt" are discarded. But if you put a string here, e.g.
-    # "Disgust", pictures labeled "Contempt" will be lumped in with "Disgust"
-    # instead of being discarded.
-    #
-    #
-    # RETURN VALUES:
-    # images, labels = List of image file paths, list of numeric labels
-    # according to EitW numbers
-    ############################################################################
-
     # Note: "Neutral" is not labeled in the CK+ dataset
     categories = ['Angry', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
     categoriesCK = ['Angry', 'Contempt', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise']
@@ -68,22 +30,17 @@ def importCKPlusDataset(dir='CK+', includeNeutral=False):
         img = label.replace(dirLabels, dirImages)
         img = img.replace('_emotion.txt', '.png')
         allLabeledImages.append(img)
-    # print(allLabeledImages)
     # Construct final set of labeled image file names and corresponding labels
     labeledImages = []
     labels = []
     labelNames = []
-    contemptImages = []
     cascade = cv2.CascadeClassifier('../data/lbpcascade_frontalface.xml')
-    # print(labelFiles)
     for ind in range(len(labelFiles)):
         curLabel = labelFiles[ind]
-        # print(allLabeledImages[ind])
         image = cv2.imread(allLabeledImages[ind])
         curImage = cv2.resize(get_largest_face(image, detect_faces(cascade, image)),
                               (224, 224),
                               interpolation=cv2.INTER_CUBIC)
-        # print(curImage)
         # Open the image as binary read-only
         with open(curLabel, 'r') as csvfile:
 
@@ -97,7 +54,6 @@ def importCKPlusDataset(dir='CK+', includeNeutral=False):
 
             # Get text label from CK+ number
             labelText = categoriesCK[numCK - 1]
-            # print(labelText)
             if labelText != 'Contempt' and labelText != 'Disgust':
                 numEitW = categories.index(labelText)
                 labeledImages.append(curImage)
@@ -172,10 +128,11 @@ def translate_labels(labels):
     pass
 
 
-input_list, labels = importDataset('../data/ck/CK')
-image = np.copy(input_list)
-labels = np.copy(labels)
-labels = np.copy(translate_labels(labels))
-print(image.shape, labels.shape)
-np.save('../data/x_train', image)
-np.save('../data/y_train', labels)
+def getNumpyArrayForTraining():
+    input_list, labels = importDataset('../data/ck/CK')
+    image = np.copy(input_list)
+    labels = np.copy(labels)
+    labels = np.copy(translate_labels(labels))
+    print(image.shape, labels.shape)
+    np.save('../data/x_train', image)
+    np.save('../data/y_train', labels)
